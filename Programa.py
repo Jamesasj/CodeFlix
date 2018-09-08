45d45fe2e6cd469ed1e76183fc9a05e734e63c82
rubiotorres <rubiotorres15@gmail.com>
2018-09-08 20:00:54 -0300

    adicionado template

--- Programa.py
+++ Programa.py
@@ -3,15 +3,15 @@ from flask import render_template
 
 app = Flask("CodeFlix")
 
-if __name__=='__main__':    
-    app.run('0.0.0.0',debug=True, port=80)
-
-@app.route('/')
+@app.route('/home')
 def home():
-    return  'ola mundo!'
+    return  render_template('home.html')
 
 def cadastro():
     return 'cadastro usuarios'
 
 def cadastrarUsuario():
     return render_template('novo_cadastro.html')
+
+if __name__=='__main__':    
+    app.run('0.0.0.0',debug=True, port=8080)
