9b86379517d125ab2bd7411ffc26c59537914d48
James Alves <james@jamesalves.com.br>
2018-09-18 18:31:20 -0300

    Login OK

--- Programa.py
+++ Programa.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, url_for
-from flask import render_template
+from flask import Flask, redirect, url_for, request,render_template, abort
+import os, json
 
 app = Flask("CodeFlix")
 
@@ -14,14 +14,29 @@ def cadastro():
 def cadastrarUsuario():
     return render_template('novo_cadastro.html')
 
-@app.route('/listar')
-def listar():
+@app.route('/listar/<folder>')
+def listar(folder):
+    print(folder)
     return render_template('lista.html')
 
-@app.route('/cadastrar')
+@app.route('/cadastrar', methods=['POST'])
 def cadastrar():
+    nomePasta = "dados/"+ request.form['senha'] + request.form['usuario']
+    if not os.path.exists(nomePasta):
+        os.makedirs(nomePasta)
+    nome = request.form['nome']
+    sobrenome = request.form['sobrenome']
+    with open(nomePasta + "\\user.json",'w') as arquivo:
+        json.dump({"nome" : nome, "sobrenome" : sobrenome } , arquivo)
     return redirect(url_for("home"))
 
+@app.route('/logar', methods=['POST'])
+def logar():
+    nomePasta = "dados/"+ request.form['senha'] + request.form['usuario']
+    if os.path.exists(nomePasta):
+        return "/listar/"+ request.form['senha'] + request.form['usuario']
+    else:
+        return abort(403)
 
 if __name__=='__main__':    
     app.run('0.0.0.0',debug=True, port=8080)
